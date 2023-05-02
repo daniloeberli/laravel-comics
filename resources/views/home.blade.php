8c2936ad3b4e3357ff307df4d139978014ca24ae
daniloeberli <danilo.eberli@gmail.com>
2023-05-02 15:41:27 +0200

    feat: comics data visualized in homepage

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,5 +3,10 @@
 @section('page.main')
     <div class="container">
         <h1>main homepage</h1>
+        <ul>
+            @foreach ($comic as $item)
+                <li> {{$item['title']}}</li>
+            @endforeach
+        </ul>
     </div>
 @endsection
--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,11 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('home');
+
+    $data = [
+        'comic' => config('comics')
+    ];
+
+
+    return view('home', $data);
 })->name('home');
