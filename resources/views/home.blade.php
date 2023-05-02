96a1803bdfab31178c623a1ae51420637f0a37fc
daniloeberli <danilo.eberli@gmail.com>
2023-05-02 20:52:51 +0200

    feat: placeholder img in comics cards

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,16 +2,20 @@
 
 @section('page.main')
     <div class="container">
-        <h1>main homepage</h1>
+        <h3>CURRENT SERIES</h3>
         <div class="d-flex justify-content-center align-items-center flex-wrap gap-5">
             @foreach ($comic as $item)
-                <div class="card col-2">
+                <div class="card col-2" style="min-height: 15rem">
+                    <img class="card-img-top" src="https://picsum.photos/300/200" alt="Card image cap">
                     <div class="card-body">
                         <p class="card-title"><strong>{{ $item['series'] }}</strong></p>
                     </div>
                 </div>
             @endforeach
-            </div>
+        </div>
+        <div class="load">
+            <a href="">LOAD MORE</a>
+        </div>
     </div>
     <section>
 
