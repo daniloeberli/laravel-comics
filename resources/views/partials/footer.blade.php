a6a8fe46d4a9bac2c4c4ce1ff87df53743eaf689
daniloeberli <danilo.eberli@gmail.com>
2023-05-02 17:12:46 +0200

    feat: footer top structure

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,3 +1,64 @@
 <footer>
-    footer
+    <section>
+        <div class="container-footer-top">
+            <ul class="list-unstyled d-flex justify-content-center align-items-center gap-4 py-4">
+                <li><img src="{{ Vite::asset('resources/img/buy-comics-digital-comics.png') }}" alt="">
+                    <span>DIGITAL COMICS</span>
+                </li>
+                <li><img src="{{ Vite::asset('resources/img/buy-comics-merchandise.png') }}" alt="">
+                    <span>DC MERCHANDISE</span>
+                </li>
+                <li><img src="{{ Vite::asset('resources/img/buy-comics-shop-locator.png') }}" alt="">
+                    <span>SUBSCRIPTIONS</span>
+                </li>
+                <li><img src="{{ Vite::asset('resources/img/buy-comics-subscriptions.png') }}" alt="">
+                    <span>COMIC SHOP LOCATOR</span>
+                </li>
+                <li><img src="{{ Vite::asset('resources/img/buy-dc-power-visa.svg') }}" alt="">
+                    <span>DC POWER VISA</span>
+                </li>
+            </ul>
+        </div>
+    </section>
 </footer>
+
+
+<style>
+    section{
+        background-color: #0282f9;
+        
+    }
+
+    .container-footer-top {
+        margin: 0 auto;
+        width: 80%;
+        padding: 50px 30px;
+        height: 160px;
+        display: flex;
+    }
+
+    ul {
+        list-style: none;
+        display: flex;
+        align-items: center;
+    }
+
+    li{
+        width: calc(100% / 5);
+        padding: 40px 20px;
+        display: flex;
+        align-items: center
+    }
+    a{
+        display: flex;
+        align-items: center;
+        text-decoration: none;
+        color: white;
+        font-size: 13px;
+    }
+
+    .img{
+        width: 50px;
+        margin-right: 20px;
+    }
+</style>
