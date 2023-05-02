dd51dceb87ff312573cac55f9f77256763062fb0
daniloeberli <danilo.eberli@gmail.com>
2023-05-02 16:19:35 +0200

    feat: header layout

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,3 +1,20 @@
 <header>
-    header
+    <div class="container">
+        <div class="d-flex align-items-center justify-content-between">
+            <div class="logo">
+                <img src="{{ Vite::asset('resources/img/dc-logo.png') }}" alt="logo" srcset="">
+            </div>
+            <nav>
+                <ul class="d-flex">
+                    <li><a href="#"></a>Prova</li>
+                    <li><a href="#"></a>Prova</li>
+                    <li><a href="#"></a>Prova</li>
+                    <li><a href="#"></a>Prova</li>
+                </ul>
+            </nav>
+            <div class="input-group mb-3">
+                <input type="text" class="form-control" aria-label="Default" aria-describedby="inputGroup-sizing-default">
+            </div>
+        </div>
+    </div>
 </header>
