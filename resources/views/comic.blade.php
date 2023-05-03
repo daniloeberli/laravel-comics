f23e224adb84827955d6ac84aa35dbed1f3cc07c
daniloeberli <danilo.eberli@gmail.com>
2023-05-03 17:49:41 +0200

    general fixes: header, comic section

--- resources/views/comic.blade.php
+++ resources/views/comic.blade.php
@@ -2,12 +2,46 @@
 
 @section('page.main')
 
-    <div class="container single-comic">
-        <h1>{{ $comic['title'] }}</h1>
-        <p>{{$comic['description']}}</p>
+    <div class="wrapper d-flex container">
+        <div class="single-comic">
+            <img class="img-fluid" src="{{$comic['thumb']}}" alt="">
+            <h1>{{ $comic['title'] }}</h1>
+            <p>{{$comic['description']}}</p>
+        </div>
+        <div >
+            <!--foto da cambiare-->
+            <img class="img-fluid" src="{{ Vite::asset('resources/img/buy-comics-digital-comics.png') }}" alt="">
+        </div>
     </div>
-    <div class="container">
-        
+    <div class="specs">
+        <div class="container d-flex justify-content-around">
+            <div class="talent w-50">
+                <h4>Talent</h4>
+                <div class="d-flex">
+                    <h5>Art by</h5>
+                    <p>artisti</p>
+                </div>
+                <div class="d-flex">
+                    <h5>Written by</h5>
+                    <p>scrittori</p>
+                </div>
+            </div>
+            <div class="specs w-50">
+                <h4>Specs</h4>
+                <div class="d-flex justify-content-between">
+                    <h5>Series</h5>
+                    <p>{{$comic['series']}}</p>
+                </div>
+                <div class="d-flex justify-content-between">
+                    <h5>US price</h5>
+                    <p>{{$comic['price']}}</p>
+                </div>
+                <div class="d-flex justify-content-between">
+                    <h5>On sale date</h5>
+                    <p>{{$comic['sale_date']}}</p>
+                </div>
+            </div>
+        </div>
     </div>
 
 @endsection
--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,18 +1,30 @@
 <header>
+    <div class="header-top">
+        <div class="container d-flex justify-content-end">
+            <div>aa</div>
+            <div>bb</div>
+        </div>
+    </div>
     <div class="container">
-        <div class="d-flex align-items-center justify-content-between">
+        <div class="d-flex align-items-center justify-content-between py-3">
             <div class="logo">
                 <img src="{{ Vite::asset('resources/img/dc-logo.png') }}" alt="logo" srcset="">
             </div>
-            <nav>
+            <nav class="px-3">
                 <ul class="d-flex">
-                    <li><a href="#"></a>Prova</li>
-                    <li><a href="#"></a>Prova</li>
-                    <li><a href="#"></a>Prova</li>
-                    <li><a href="#"></a>Prova</li>
+                    <li><a href="#"></a>Characters</li>
+                    <li><a href="{{route('comics')}}"></a>Comics</li>
+                    <li><a href="#"></a>Movies</li>
+                    <li><a href="#"></a>TV</li>
+                    <li><a href="#"></a>Games</li>
+                    <li><a href="#"></a>Collectibles</li>
+                    <li><a href="#"></a>Videos</li>
+                    <li><a href="#"></a>Fans</li>
+                    <li><a href="#"></a>News</li>
+                    <li><a href="#"></a>Shop</li>
                 </ul>
             </nav>
-            <div class="input-group mb-3">
+            <div class="input-group mb-3" style="width: 10%">
                 <input type="text" class="form-control" aria-label="Default" aria-describedby="inputGroup-sizing-default">
             </div>
         </div>
